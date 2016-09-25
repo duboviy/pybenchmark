98a0568a595de9495e3974869af82552fae0e183
Eugene Dubovoy <eugene.dubovoy@gmail.com>
2016-09-25 23:18:37 +0300

    Resolved issue #5: Add Tracking Memory Usage
    Using Heapy from Guppy-PE Primer.
    
    Guppy-PE (http://guppy-pe.sourceforge.net) is a framework that provides a
    memory profiler called Heap, among other features.

--- pybenchmark/__init__.py
+++ pybenchmark/__init__.py
@@ -1,16 +1,12 @@
-"""
-Performance utility functional used in performance tests.
-
-When measuring execution time, the result depends on the computer hardware.
-To be able to produce a universal measure, the simplest way is to benchmark the
-speed of a fixed sequence of code and calculate a ratio out of it. From there, the time
-taken by a function can be translated to a universal value that can be compared on
-any computer. Python provides a benchmark utility in its test package that measures the duration
-of a sequence of well-chosen operations.
-"""
+""" A benchmark utility used in speed/performance tests. """
 import time
 import sys
 from test import pystone    # native python-core "PYSTONE" Benchmark Program
+
+# A Guppy-PE Primer (http://guppy-pe.sourceforge.net) is a third party framework that provides a
+# memory profiler called Heap, among other features.
+# Installed by requirments.txt. Custom installation >>> pip install guppy
+from guppy import hpy
 
 # same condition in timeit (getting wall-time -> not process time)
 timer = time.clock if sys.platform == 'win32' else time.time
@@ -20,25 +16,40 @@ timer = time.clock if sys.platform == 'win32' else time.time
 # and the time used to perform the benchmark, result depends on the hardware.
 BENCHTIME, PYSTONES = pystone.pystones()
 KPYSTONES = PYSTONES / 1000.0
-PERFORMANCE_STATS = {}
+STATS = {}
+
+_INIT_MEM_SIZE = 12     # 12 corresponds to the initial memory size after a setref call
 
 
-def duration(name='stats', stats=PERFORMANCE_STATS):
-    def _duration(function):
-        def __duration(*args, **kw):
+def profile(name='stats', stats=STATS):
+    """Calculates a duration and a memory size."""
+    def _profile(function):
+        def __profile(*args, **kw):
             start_time = timer()
+            profiler = hpy()
+            profiler.setref()
+            start = profiler.heap().size + _INIT_MEM_SIZE
             try:
                 return function(*args, **kw)
             finally:
                 total = timer() - start_time
                 kstones = _seconds_to_kpystones(total)
-                stats[name] = {'total_time': total, 'kilo_stones': kstones}
-
-        return __duration
-
-    return _duration
+                memory = profiler.heap().size - start
+                stats[name] = {'time': total,
+                               'kstones': kstones,
+                               'memory': memory}
+        return __profile
+    return _profile
 
 
 def _seconds_to_kpystones(seconds):
     """ Return pystones amount of time performing operations. """
     return (PYSTONES * seconds) / 1000
+
+
+if __name__ == '__main__':
+    """ Example of usage. """
+    some_code = lambda: 2 ** 1000000    # callable that will be decorated and measured
+    decorated = profile('example_of_usage')(some_code)  # a la-carte decoration
+    please = decorated()                # actual run/call of decorated callable
+    print(STATS)
