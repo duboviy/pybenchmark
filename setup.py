4283035101ec8423126c340423be74d73f9b4184
Yevgen Dubovyi <eugene.dubovoy@gmail.com>
2016-10-03 12:29:56 +0300

    Add a new version to put repo on PyPI

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pybenchmark',
   packages = ['pybenchmark'], # this must be the same as the name above
-  version = '0.0.3',
+  version = '0.0.4',
   description = 'A benchmark utility used in performance tests.',
   author = 'Eugene Duboviy',
   author_email = 'eugene.dubovoy@gmail.com',
   url = 'https://github.com/duboviy/pybenchmark', # use the URL to the github repo
-  download_url = 'https://github.com/duboviy/pybenchmark/tarball/0.0.3', # I'll explain this in a second
+  download_url = 'https://github.com/duboviy/pybenchmark/tarball/0.0.4', # I'll explain this in a second
   keywords = ['benchmark', 'performance', 'testing'], # arbitrary keywords
   classifiers = [],
 )
