434bc5554dcd8b2f2bf4a3b4a1d1991746e86b78
Yevgen Dubovyi <eugene.dubovoy@gmail.com>
2016-10-04 10:09:29 +0300

    Fix long description; Add new PyPI release version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pybenchmark',
   packages = ['pybenchmark'], # this must be the same as the name above
-  version = '0.0.5',
+  version = '0.0.6',
   description = 'A benchmark utility used in performance tests.',
   author = 'Eugene Duboviy',
   author_email = 'eugene.dubovoy@gmail.com',
   url = 'https://github.com/duboviy/pybenchmark', # use the URL to the github repo
-  download_url = 'https://github.com/duboviy/pybenchmark/tarball/0.0.5', # I'll explain this in a second
+  download_url = 'https://github.com/duboviy/pybenchmark/tarball/0.0.6', # I'll explain this in a second
   keywords = ['benchmark', 'performance', 'testing'], # arbitrary keywords
   classifiers=[
     "Programming Language :: Python",
@@ -17,7 +17,14 @@ setup(
     "Operating System :: OS Independent",
     "Topic :: Software Development :: Libraries :: Python Modules",
   ],
-  long_description=open('README.md').read(),
+  long_description="""
+When measuring execution time, the result depends on the computer hardware.
+To be able to produce a universal measure, the simplest way is to benchmark the speed of a fixed sequence
+of code and calculate a ratio out of it. From there, the time taken by a function can be translated to a
+universal value that can be compared on any computer. Python provides a benchmark utility in its test
+package that measures the duration of a sequence of well-chosen operations.
+pybenchmark designed to provide a simple and pythonic way to get performance data.
+""",
   install_requires=[
     'psutil',
     'gevent',
