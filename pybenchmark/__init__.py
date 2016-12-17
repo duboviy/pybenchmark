6e663388e1ba830a99af5694609c8546ecb5a06d
Eugene Duboviy <eugene.dubovoy@gmail.com>
2016-12-17 21:02:44 +0200

    Update __init__.py
    Remove redundant part of imports

--- pybenchmark/__init__.py
+++ pybenchmark/__init__.py
@@ -1,10 +1,4 @@
-try:                    # py 2
-    from profile import profile, stats, kpystones, pystones
-    from meminfo import MemInfo
-    from cpuinfo import CpuInfo
-    from .gprofiler import GProfiler
-except ImportError:     # py 3
-    from .profile import profile, stats, kpystones, pystones
-    from .meminfo import MemInfo
-    from .cpuinfo import CpuInfo
-    from .gprofiler import GProfiler
+from .profile import profile, stats, kpystones, pystones
+from .meminfo import MemInfo
+from .cpuinfo import CpuInfo
+from .gprofiler import GProfiler
